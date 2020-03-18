e6136860a76d94f11302ac695160b96a71835fb3
Chendo Monster <PS196856@summacollege.nl>
2020-03-18 13:34:46 +0100

    een extra regel code

--- github_week6/Program.cs
+++ github_week6/Program.cs
@@ -7,6 +7,7 @@ namespace github_week6
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Extra regel");
         }
     }
 }
